2e11f79eaaa1deccdf7e327262e996b689ac7819
段辉 <554112075@qq.com>
2020-04-13 09:19:44 +0800

    令牌桶限流

--- src/main/java/com/dfy/ratelimiter/controller/TestController.java
+++ src/main/java/com/dfy/ratelimiter/controller/TestController.java
@@ -2,6 +2,7 @@ package com.dfy.ratelimiter.controller;
 
 import com.dfy.ratelimiter.core.FixedWindowCounterLimit;
 import com.dfy.ratelimiter.core.SlidingWindowCounterLimit;
+import com.dfy.ratelimiter.core.TokenBucketLimit;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +18,13 @@ public class TestController {
 
     private FixedWindowCounterLimit fixedWindowCounterLimit = new FixedWindowCounterLimit(10, 10);
 
-    private SlidingWindowCounterLimit slidingWindowCounterLimit = new SlidingWindowCounterLimit(20, 10, 10);
+//    private SlidingWindowCounterLimit slidingWindowCounterLimit = new SlidingWindowCounterLimit(20, 10, 10);
+
+    private TokenBucketLimit tokenBucketLimit = new TokenBucketLimit(2, 1, 50);
 
     @GetMapping("/hello")
     public String hello() {
-        if (!slidingWindowCounterLimit.tryCount()) {
+        if (!tokenBucketLimit.tryAcquire()) {
             return "限流!";
         }
         return "hello world!";
